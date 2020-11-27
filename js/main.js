27874b0771f1efa3717d1e9978a8e8940905bf42
Ahmed Osama <ahmedosamabedawy@gmail.com>
2020-11-28 01:29:34 +0200

    Add Preloader page

--- js/main.js
+++ js/main.js
@@ -41,20 +41,10 @@ scrollButton.on('click', function () {
     }, 600);
 });
 
-(function($) {
-	
-	"use strict";
-	
-	//Hide Loading Box (Preloader)
-	function handlePreloader() {
-		if($('.loader-wrap').length){
-			$('.loader-wrap').delay(1000).fadeOut(500);
-		}
-	}
+$(function () {
+    'use strict';
 
-	if ($(".preloader-close").length) {
-        $(".preloader-close").on("click", function(){
-            $('.loader-wrap').delay(200).fadeOut(500);
-        })
-    }
+    $(window).on('load', function () {
+        $(".loader").delay(600).fadeOut();
+    });
 });
