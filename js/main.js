9be3f6d4a3957fce83ee418043dc7511f107bf76
Ahmed Osama <ahmedosamabedawy@gmail.com>
2020-11-27 23:19:05 +0200

    edit

--- js/main.js
+++ js/main.js
@@ -25,3 +25,36 @@ $('.stop').on('click', function () {
     owl.trigger('stop.owl.autoplay')
 
 })
+
+// Caching The Scroll Top Element
+var scrollButton = $("#scroll-top");
+
+$(window).scroll(function () {
+    if ($(this).scrollTop() >= 200) scrollButton.show();
+    else scrollButton.hide();
+});
+
+// Click On Button To Scroll Top
+scrollButton.on('click', function () {
+    $('body,html').animate({
+        scrollTop: 0
+    }, 600);
+});
+
+(function($) {
+	
+	"use strict";
+	
+	//Hide Loading Box (Preloader)
+	function handlePreloader() {
+		if($('.loader-wrap').length){
+			$('.loader-wrap').delay(1000).fadeOut(500);
+		}
+	}
+
+	if ($(".preloader-close").length) {
+        $(".preloader-close").on("click", function(){
+            $('.loader-wrap').delay(200).fadeOut(500);
+        })
+    }
+});
